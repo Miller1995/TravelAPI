12731bd22a44f60271ffba647585118f381b420c
miller1995 <nircaanton95@gmail.com>
2022-11-09 18:29:26 +0200

    -  created new spring boot project "travel"; - created new database Postgresql "travel"; - add dependency (validation, web, data Jpa, postgresql, lombok); - connected DB; - Created table "Travel";

---
+++ travel/src/main/java/md/miller1995/travel/TravelApplication.java
@@ -0,0 +1,13 @@
+package md.miller1995.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelApplication.class, args);
+	}
+
+}
---
+++ travel/src/test/java/md/miller1995/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package md.miller1995.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
